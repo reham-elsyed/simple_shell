494ceb4a9d47a635b7b4b8781ba9bb366a245f05
reham-elsyed <rehamshepl666@gmail.com>
2023-12-10 12:30:31 -0800

    strtok added

--- ar_strtok.c
+++ ar_strtok.c
@@ -7,5 +7,39 @@
  */
 char *ar_strtok(char *str, const char *delim)
 {
-	return (strtok(str, delim));
+	static char *s = NULL;
+	char *start = NULL;
+
+	if (str == NULL)
+	{
+		str = s;
+		if (s == NULL)
+		{
+			return (NULL);
+		}
+	}
+	start = str;
+	while (*str != '\0' && *str == *delim)
+	{
+		str++;
+	}
+		if (*str == '\0')
+		{
+			s = NULL;
+			return (NULL);
+		}
+		while (*str != '\0' && *str != *delim)
+		{
+			str++;
+		}
+		if (*str != '\0')
+		{
+			*str = '\0';
+			s = str + 1;
+		}
+		else
+		{
+			s = NULL;
+		}
+		return (start);
 }
