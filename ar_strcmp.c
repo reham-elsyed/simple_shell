cbae4704ceff3e900115f318d0f84e1dd7001c90
ahmed elkfafy <hmdeslam.adel@gmail.com>
2023-12-13 19:33:12 +0200

    fourth_try

--- ar_strcmp.c
+++ ar_strcmp.c
@@ -7,5 +7,14 @@
  */
 int ar_strcmp(const char *s1, const char *s2)
 {
-	return (strcmp(s1, s2));
+	int sum = 0;
+	int i, s1_len = ar_strlen(s1), s2_len = ar_strlen(s2);
+
+	for (i = 0; i < s1_len && i < s2_len; i++)
+		sum += s1[i] - s2[i];
+	for (; i < s1_len; i++)
+		sum += s1[i];
+	for (; i < s2_len; i++)
+		sum += s2[i];
+	return (sum);
 }
--- main.h
+++ main.h
@@ -5,7 +5,6 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
